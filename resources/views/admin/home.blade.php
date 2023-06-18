799cb48ac7be5fba2c1ed0a83167e5090be04ad6
CosimoTramonte <cosimotramonte7@gmail.com>
2023-06-18 18:54:56 +0200

    fix color text dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -6,7 +6,7 @@
         HOME Dashboard
     </h2>
 
-    <p>
+    <p class="text-white">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt tempora exercitationem facere expedita animi excepturi enim impedit pariatur, nisi blanditiis quia corrupti earum doloremque fugit optio, aut soluta libero necessitatibus! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, tempore alias quisquam necessitatibus exercitationem possimus rem distinctio quod aperiam soluta aliquam. Voluptatibus architecto suscipit culpa praesentium ab veritatis, animi delectus.
     </p>
 
